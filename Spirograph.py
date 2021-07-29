0d88a40fa7d14c64d0b70d02879ced767a316128
Pikachu-done <88133614+Pikachu-done@users.noreply.github.com>
2021-07-29 15:42:31 +0530

    Create Spirograph.py

---
+++ Spirograph.py
@@ -0,0 +1,16 @@
+import turtle as tt
+
+tt.pensize(2)
+tt.bgcolor("black")
+tt.speed(50)
+
+for i in range(12):
+     for color in ('red', 'magenta', 'blue', 
+                  'cyan', 'green', 'white',
+                  'yellow'):
+        tt.color(color)
+          
+
+        tt.circle(100)
+        tt.lt(10)
+        tt.h
