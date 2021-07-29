e6533c16a82ac4b47f20889e180f96e7249c1230
Pikachu-done <88133614+Pikachu-done@users.noreply.github.com>
2021-07-29 16:24:36 +0530

    Create Rainbow.py

---
+++ Rainbow.py
@@ -0,0 +1,28 @@
+import turtle
+
+sc = turtle.Screen()
+
+pen = turtle.Turtle()
+
+def semi_circle(col,rad,val):
+    pen.color(col)
+    pen.circle(rad,-180)
+    pen.up()
+    pen.setpos(val,0)
+    pen.down()
+    pen.right(180)
+
+col = ["violet","indigo","blue","green","yellow","orange","red"]
+sc.setup(600,600)
+
+sc.bgcolor("black")
+
+pen.right(90)
+pen.width(15)
+pen.speed(11)
+turtle.ht()
+
+for i in range(7):
+    semi_circle(col[i],10*(i+20), -10*(i+1))
+
+turtle.done()
