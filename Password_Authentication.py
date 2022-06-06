8b1349bd53d6df4f3e3a1109111b43a64da787db
Pikachus-code <88133614+Pikachus-code@users.noreply.github.com>
2022-06-06 16:46:48 +0530

    Password_Authentication

---
+++ Password_Authentication.py
@@ -0,0 +1,10 @@
+import getpass
+database = {"Tanish.Nahar": "123456", "Nahar.Tanish": "654321"}
+username = input("Enter Your Username : ")
+password = getpass.getpass("Enter Your Password : ")
+for i in database.keys():
+    if username == i:
+        while password != database.get(i):
+            password = getpass.getpass("Enter Your Password Again : ")
+        break
+print("Verified")
